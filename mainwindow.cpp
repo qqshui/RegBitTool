1238cb6aa4eeb38b720777f4dfb7d9d68d8a1a04
fangdkikui <fangdikui@163.com>
2020-01-17 22:08:14 -0800

    add src

---
+++ mainwindow.cpp
@@ -0,0 +1,253 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    init();
+    initSignalMap();
+    setRegExpValidator();
+    updateUI(0);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete signalMapper;
+    delete hexNumRegExpValidator;
+    delete binNumRegExpValidator;
+    delete decNumRegExpValidator;
+}
+
+void MainWindow::init()
+{
+    buttonList.append(ui->toolButton_00);
+    buttonList.append(ui->toolButton_01);
+    buttonList.append(ui->toolButton_02);
+    buttonList.append(ui->toolButton_03);
+    buttonList.append(ui->toolButton_04);
+    buttonList.append(ui->toolButton_05);
+    buttonList.append(ui->toolButton_06);
+    buttonList.append(ui->toolButton_07);
+    buttonList.append(ui->toolButton_08);
+    buttonList.append(ui->toolButton_09);
+    buttonList.append(ui->toolButton_10);
+    buttonList.append(ui->toolButton_011);
+    buttonList.append(ui->toolButton_012);
+    buttonList.append(ui->toolButton_013);
+    buttonList.append(ui->toolButton_014);
+    buttonList.append(ui->toolButton_015);
+    buttonList.append(ui->toolButton_016);
+    buttonList.append(ui->toolButton_017);
+    buttonList.append(ui->toolButton_018);
+    buttonList.append(ui->toolButton_019);
+    buttonList.append(ui->toolButton_020);
+    buttonList.append(ui->toolButton_021);
+    buttonList.append(ui->toolButton_022);
+    buttonList.append(ui->toolButton_023);
+    buttonList.append(ui->toolButton_024);
+
+    buttonList.append(ui->toolButton_025);
+    buttonList.append(ui->toolButton_026);
+    buttonList.append(ui->toolButton_027);
+    buttonList.append(ui->toolButton_028);
+    buttonList.append(ui->toolButton_029);
+    buttonList.append(ui->toolButton_030);
+    buttonList.append(ui->toolButton_031);
+
+    for(int i=0;i<32;i++){
+        buttonList[i]->setCheckable(true);
+    }
+
+}
+
+void MainWindow::updateUI(int32_t value)
+{
+    //for bit button
+    for(int i=0;i<32;i++){
+        if((value >> i) & 0x1){
+            buttonList[i]->setChecked(true);
+            buttonList[i]->setText("1");
+        }else{
+            buttonList[i]->setChecked(false);
+            buttonList[i]->setText("0");
+        }
+    }
+
+    //for hex
+    char chBuf[8];
+    sprintf(chBuf,"%08x",value);
+    QString hex = QString::fromUtf8(chBuf);
+    ui->lineEdit_hex->setText(hex);
+
+    QString dec = QString::number( value );
+    ui->lineEdit_dec->setText(dec);
+
+    //for bin
+    QString bin = QString::number( value ,2);
+
+    //add prefix 0
+    int binlen = bin.length();
+    for(int i=0;i < 32-binlen;i++){
+        bin.insert(0,"0");
+    }
+    //add space per 4 bit
+    QString formatbin ;
+    for(int i=0;i < 32;i+=4){
+        formatbin.append( bin.mid(i,4) );
+        formatbin.append(" ");
+    }
+    formatbin = formatbin.trimmed();
+    ui->lineEdit_bin->setText(formatbin);
+
+
+}
+
+void MainWindow::updateBitButton(int32_t value)
+{
+
+    //for bit button
+    for(int i=0;i<32;i++){
+        if((value >> i) & 0x1){
+            buttonList[i]->setChecked(true);
+            buttonList[i]->setText("1");
+        }else{
+            buttonList[i]->setChecked(false);
+            buttonList[i]->setText("0");
+        }
+    }
+
+}
+
+void MainWindow::updateHexLineEdit(int32_t value)
+{
+    //for hex
+    char chBuf[8];
+    sprintf(chBuf,"%08x",value);
+    QString hex = QString::fromUtf8(chBuf);
+    ui->lineEdit_hex->setText(hex);
+
+
+}
+
+void MainWindow::updateDecLineEdit(int32_t value)
+{
+    QString dec = QString::number( value );
+    ui->lineEdit_dec->setText(dec);
+}
+
+void MainWindow::updateBinLineEdit(int32_t value)
+{
+    //for bin
+    QString bin;
+    for(int i=0;i<32;i++){
+        if((value >> i) & 0x1){
+            bin.insert(0,"1");
+        }else{
+            bin.insert(0,"0");
+        }
+    }
+    //add space per 4 bit
+    QString formatbin ;
+    for(int i=0;i < 32;i+=4){
+        formatbin.append( bin.mid(i,4) );
+        formatbin.append(" ");
+    }
+    formatbin = formatbin.trimmed();
+
+    ui->lineEdit_bin->setText(formatbin);
+
+}
+
+
+
+void MainWindow::on_lineEdit_hex_textChanged(const QString &)
+{
+    if(! ui->lineEdit_hex->hasFocus()){
+        return;
+    }
+    bool ok;
+    uint uintRegVal = ui->lineEdit_hex->text().toUInt(&ok,16);
+    regval = uintRegVal;
+    updateDecLineEdit(regval);
+    updateBinLineEdit(regval);
+    updateBitButton(regval);
+}
+
+void MainWindow::initSignalMap()
+{
+    signalMapper = new QSignalMapper(this);
+
+    for (int i = 0; i < buttonList.count(); ++i) {
+        connect(buttonList[i], SIGNAL(clicked()), signalMapper, SLOT(map()));
+        signalMapper->setMapping(buttonList[i], i);
+    }
+    connect(signalMapper, SIGNAL(mapped(int)), this, SLOT(clickButton(int)));
+}
+
+
+void MainWindow::clickButton(int id){
+
+    if(buttonList[id]->isChecked()){
+        regval |= 1 << id;
+    }else{
+        regval &= ~(1 << id);
+    }
+    updateBitButton(regval);
+    updateHexLineEdit(regval);
+    updateDecLineEdit(regval);
+    updateBinLineEdit(regval);
+}
+void MainWindow::setRegExpValidator(){
+    QRegExp regExp("[a-fA-F0-9]{8}");
+    hexNumRegExpValidator = new QRegExpValidator(regExp, this);
+    ui->lineEdit_hex->setValidator(hexNumRegExpValidator);
+
+    QRegExp binRegExp("[01]{32}");
+    binNumRegExpValidator = new QRegExpValidator(binRegExp, this);
+    ui->lineEdit_bin->setValidator(binNumRegExpValidator);
+
+    QRegExp decRegExp("[0-9]{32}");
+    decNumRegExpValidator = new QRegExpValidator(decRegExp, this);
+    ui->lineEdit_dec->setValidator(decNumRegExpValidator);
+
+}
+
+
+
+void MainWindow::on_lineEdit_bin_textChanged(const QString &)
+{
+    if(! ui->lineEdit_bin->hasFocus()){
+        return;
+    }
+    QString formatBin = ui->lineEdit_bin->text();
+    QString bin;
+    for(int i=0;i<formatBin.length();i++){
+        if(formatBin.at(i) == QChar('0') || formatBin.at(i) == QChar('1') ){
+            bin.append(formatBin.at(i) );
+        }
+    }
+    bool ok;
+    regval = bin.toUInt(&ok,2);
+
+    updateBitButton(regval);
+    updateHexLineEdit(regval);
+    updateDecLineEdit(regval);
+}
+
+void MainWindow::on_lineEdit_dec_textChanged(const QString &)
+{
+    if(! ui->lineEdit_dec->hasFocus()){
+        return;
+    }
+    QString dec = ui->lineEdit_dec->text();
+
+    bool ok;
+    regval = dec.toUInt(&ok,10);
+
+    updateBitButton(regval);
+    updateHexLineEdit(regval);
+    updateBinLineEdit(regval);
+}
---
+++ mainwindow.h
@@ -0,0 +1,51 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QToolButton>
+#include <QSignalMapper>
+#include <QRegExpValidator>
+#include <QRegExp>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void initSignalMap();
+private slots:
+
+    void on_lineEdit_hex_textChanged(const QString &);
+
+
+    void on_lineEdit_bin_textChanged(const QString &);
+
+    void on_lineEdit_dec_textChanged(const QString &);
+
+    void clickButton(int id);
+
+private:
+    Ui::MainWindow *ui;
+    int32_t regval;
+    QVector<QToolButton *> buttonList;
+    void init();
+    void updateUI(int32_t value);
+    void updateBitButton(int32_t value);
+    void updateHexLineEdit(int32_t value);
+    void updateDecLineEdit(int32_t value);
+    void updateBinLineEdit(int32_t value);
+    QSignalMapper *signalMapper;
+    QRegExpValidator *hexNumRegExpValidator;
+    QRegExpValidator *binNumRegExpValidator;
+    QRegExpValidator *decNumRegExpValidator;
+    void setRegExpValidator();
+
+};
+
+#endif // MAINWINDOW_H
